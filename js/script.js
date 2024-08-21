501115a73fc726c73e312a835d931d52f68cc651
portifolio-html <evaldogusmaocosta@outlook.com>
2024-08-20 22:27:25 -0300

    ok

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var menu = document.getElementById("menu-hamburgue")
+var list = document.getElementById("menu-list-id")
 
 menu.addEventListener("click", () => {
     menu.classList.toggle("menu-on")
+    list.classList.toggle("menu-list-on")
 })
